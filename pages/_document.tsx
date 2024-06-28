31bcbd3f9c564cbdd4e583652bd1d593d4eb1015
Biraj <70630924+birajrai@users.noreply.github.com>
2024-06-28 23:52:49 +0545

    feat: added title & description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,25 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="icon" href="/favicon.ico" />
+                    <title>To-Do App</title>
+                    <meta
+                        name="description"
+                        content="A modern, responsive To-Do app built with Next.js, TailwindCSS, and TypeScript. The app supports dark mode and allows users to manage tasks with priority levels and due dates."
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
 
 export default MyDocument;
