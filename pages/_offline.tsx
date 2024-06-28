e05adfb7969fbe1bd2d525005c9f1e7a97d1154f
Biraj <70630924+birajrai@users.noreply.github.com>
2024-06-28 22:36:14 +0545

    Update _offline.tsx

---
+++ pages/_offline.tsx
@@ -0,0 +1,11 @@
+const Offline: React.FC = () => {
+    return (
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl">You are offline</h1>
+        <p>Please check your internet connection.</p>
+      </div>
+    );
+  };
+  
+  export default Offline;
+  
