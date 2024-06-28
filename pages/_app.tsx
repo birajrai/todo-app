c96cca82e9385e4288e945b21b3c79e2c2c45f59
Biraj <70630924+birajrai@users.noreply.github.com>
2024-06-28 23:40:02 +0545

    feat: import global CSS and implement dark mode toggle with icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,39 +2,43 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { TaskProvider } from '../contexts/TaskContext';
 import { useState, useEffect } from 'react';
+import { FaSun, FaMoon } from 'react-icons/fa';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [darkMode, setDarkMode] = useState(false);
+    const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    const isDarkMode = localStorage.getItem('darkMode') === 'true';
-    setDarkMode(isDarkMode);
-    if (isDarkMode) {
-      document.documentElement.classList.add('dark');
-    }
-  }, []);
+    useEffect(() => {
+        const isDarkMode = localStorage.getItem('darkMode') === 'true';
+        setDarkMode(isDarkMode);
+        if (isDarkMode) {
+            document.documentElement.classList.add('dark');
+        }
+    }, []);
 
-  const toggleDarkMode = () => {
-    const newDarkMode = !darkMode;
-    setDarkMode(newDarkMode);
-    localStorage.setItem('darkMode', newDarkMode.toString());
-    if (newDarkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  };
+    const toggleDarkMode = () => {
+        const newDarkMode = !darkMode;
+        setDarkMode(newDarkMode);
+        localStorage.setItem('darkMode', newDarkMode.toString());
+        if (newDarkMode) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    };
 
-  return (
-    <TaskProvider>
-      <div className="flex justify-end p-4">
-        <button onClick={toggleDarkMode} className="bg-nord-3 dark:bg-nord-2 text-nord-6 dark:text-nord-0 p-2 rounded">
-          Toggle {darkMode ? 'Light' : 'Dark'} Mode
-        </button>
-      </div>
-      <Component {...pageProps} />
-    </TaskProvider>
-  );
+    return (
+        <TaskProvider>
+            <div className="flex justify-end p-4 bg-nord-3 dark:bg-nord-2 text-nord-6 dark:text-nord-0">
+                <button
+                    onClick={toggleDarkMode}
+                    className="bg-nord-3 dark:bg-nord-2 text-nord-6 dark:text-nord-0 p-2 rounded"
+                >
+                    {darkMode ? <FaSun /> : <FaMoon />}
+                </button>
+            </div>
+            <Component {...pageProps} />
+        </TaskProvider>
+    );
 }
 
 export default MyApp;
