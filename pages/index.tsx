e216d82a34089c511bed8fb4c580650b43aa4849
Biraj <70630924+birajrai@users.noreply.github.com>
2024-06-28 22:36:10 +0545

    Update index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import TaskList from '../components/TaskList';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <TaskList />
+    </div>
+  );
+};
+
+export default Home;
