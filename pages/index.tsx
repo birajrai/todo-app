901f01f22fcab7f84b32599e54594a62061456e4
Biraj <70630924+birajrai@users.noreply.github.com>
2024-06-28 23:13:22 +0545

    feat: apply Nord theme and dark mode support to the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import TaskList from '../components/TaskList';
 
 const Home: React.FC = () => {
   return (
-    <div className="container mx-auto p-4">
+    <div className="min-h-screen bg-nord-6 dark:bg-nord-0 text-nord-0 dark:text-nord-6">
       <TaskList />
     </div>
   );
